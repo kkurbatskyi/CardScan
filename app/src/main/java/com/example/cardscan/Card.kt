f4b333ee77ca076ef7dc0767c958800c4bc16ddd
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2021-07-20 22:20:49 -0700

    Update Card.kt

--- app/src/main/java/com/example/cardscan/Card.kt
+++ app/src/main/java/com/example/cardscan/Card.kt
@@ -1,4 +1,17 @@
 package com.example.cardscan
 
-class Card {
+import java.net.URL
+
+//Contains all the information from business cards
+class Card(fname: String, lname: String, phoneNumber: String, companyName: String, website: URL) {
+    fun formatName(){
+
+    }
+    fun formatNumber(){
+
+    }
+    fun formatWebsite(){
+
+    }
+    fun generateBarcode(){}
 }
