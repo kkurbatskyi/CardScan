2df98965a687ee620841f9a45e62362216ae233c
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2021-07-21 22:59:08 -0700

    Implemented Card::formatNumber

--- app/src/main/java/com/example/cardscan/Card.kt
+++ app/src/main/java/com/example/cardscan/Card.kt
@@ -4,11 +4,17 @@ import java.net.URL
 
 //Contains all the information from business cards
 class Card(fname: String, lname: String, phoneNumber: String, companyName: String, website: URL) {
+    lateinit var phoneNumber: String
+
     fun formatName(){
 
     }
     fun formatNumber(){
-
+        var newNumber = ""
+        for(i in phoneNumber){
+            newNumber += if(i.isDigit()) i else ""
+        }
+        phoneNumber = newNumber
     }
     fun formatWebsite(){
 
